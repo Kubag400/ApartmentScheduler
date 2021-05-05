9aa7f05a14aaeb38d090ecbd1a224560966e15d9
Jakub Gawęda <kubag98@hotmail.com>
2021-05-05 11:10:40 +0200

    Add BasicInstaller in Extensions

---
+++ ApartmentScheduler/Extensions/BasicInstaller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApartmentScheduler.Extensions
+{
+    public class BasicInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddRazorPages();
+            services.AddControllersWithViews();
+        }
+    }
+}
--- ApartmentScheduler/Extensions/InstallerExtensions.cs
+++ ApartmentScheduler/Extensions/InstallerExtensions.cs
@@ -9,7 +9,7 @@ namespace ApartmentScheduler.Extensions
 {
     public static class InstallerExtensions
     {
-        public static void InstallServicesAssembly(IServiceCollection service,IConfiguration configuration)
+        public static void InstallServicesAssembly(IServiceCollection services,IConfiguration configuration)
         {
             var installers = typeof(Startup).Assembly.ExportedTypes.Where(x =>
             typeof(IInstaller).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
