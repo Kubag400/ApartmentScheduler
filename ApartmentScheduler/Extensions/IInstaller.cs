1e260d726ec8fb71394ef0e0a262dd2465e30c6a
Jakub Gawęda <kubag98@hotmail.com>
2021-05-05 11:04:44 +0200

    Add InstallServiceassembly in Extensions

--- ApartmentScheduler/Extensions/IInstaller.cs
+++ ApartmentScheduler/Extensions/IInstaller.cs
@@ -1,14 +1,10 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ApartmentScheduler.Extensions
 {
     public interface IInstaller
     {
-        void InstallInAssembly(IServiceCollection services, IConfiguration configuration);
+        void InstallServices(IServiceCollection services, IConfiguration configuration);
     }
 }
