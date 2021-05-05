edb69db6b3b2fd2ae521eef9d3558b5e27debc59
Jakub Gawęda <kubag98@hotmail.com>
2021-05-05 11:01:35 +0200

    Add IInstaller in Extensions

---
+++ ApartmentScheduler/Extensions/IInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApartmentScheduler.Extensions
+{
+    public interface IInstaller
+    {
+        void InstallInAssembly(IServiceCollection services, IConfiguration configuration);
+    }
+}
